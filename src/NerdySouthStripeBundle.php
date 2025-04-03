165e8aea24db047d397072422a7830c0743f18bd
Brandon DuBois <skrilled@gmail.com>
2025-04-03 12:43:34 -0400

    test loading route like this

--- src/NerdySouthStripeBundle.php
+++ src/NerdySouthStripeBundle.php
@@ -2,18 +2,15 @@
 
 namespace NerdySouth\StripeBundle;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class NerdySouthStripeBundle extends Bundle
 {
-    /**
-     * Load the routes for this bundle.
-     */
     public function configureRoutes(RoutingConfigurator $routes): void
     {
-        // Import your routes from config/routes.yaml in the bundle
-        $routes->import(__DIR__ . '/Resources/config/routes.yaml');
+        $routes->add('nerdysouth_stripe_webhook', '/webhook/stripe')
+            ->controller('NerdySouth\StripeBundle\Controller\StripeWebhookController::handleWebhook')
+            ->methods(['POST']);
     }
 }
