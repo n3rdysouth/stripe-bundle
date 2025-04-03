32e4223310c9f8fc6ad4a571f6f3c36940983492
Brandon DuBois <skrilled@gmail.com>
2025-04-03 14:44:05 -0400

    testing more

--- src/DependencyInjection/NerdySouthStripeExtension.php
+++ src/DependencyInjection/NerdySouthStripeExtension.php
@@ -11,6 +11,11 @@ class NerdySouthStripeExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
+        // Define default parameters directly from environment variables
+        $container->setParameter('nerdysouth_stripe.api_key', '%env(STRIPE_SECRET_KEY)%');
+        $container->setParameter('nerdysouth_stripe.webhook_secret', '%env(STRIPE_WEBHOOK_SECRET)%');
+
+        // Load the service definitions from services.yaml
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
         $loader->load('services.yaml');
     }
