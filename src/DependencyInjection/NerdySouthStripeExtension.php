5cce39591a884aebcaaa81611bb7abc7fc8ddf25
Brandon DuBois <skrilled@gmail.com>
2025-04-03 14:37:46 -0400

    tesitng

--- src/DependencyInjection/NerdySouthStripeExtension.php
+++ src/DependencyInjection/NerdySouthStripeExtension.php
@@ -2,7 +2,6 @@
 
 namespace NerdySouth\StripeBundle\DependencyInjection;
 
-use Symfony\Bundle\FrameworkBundle\DependencyInjection\Configuration;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\Config\FileLocator;
@@ -12,15 +11,6 @@ class NerdySouthStripeExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
-        // Process and validate user-defined bundle configuration
-        $configuration = new Configuration('nerdysouth_stripe');
-        $config = $this->processConfiguration($configuration, $configs);
-
-        // Set parameters for use in services
-        $container->setParameter('nerdysouth_stripe.api_key', '%env(STRIPE_SECRET_KEY)%');
-        $container->setParameter('nerdysouth_stripe.webhook_secret', '%env(STRIPE_WEBHOOK_SECRET)%');
-
-        // Load service definitions
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
         $loader->load('services.yaml');
     }
