a2d211db94c743fb21f0a892c8f0e50fe9df7407
Brandon DuBois <skrilled@gmail.com>
2025-04-03 12:35:30 -0400

    updatest for testing

--- src/NerdySouthStripeBundle.php
+++ src/NerdySouthStripeBundle.php
@@ -2,8 +2,18 @@
 
 namespace NerdySouth\StripeBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 class NerdySouthStripeBundle extends Bundle
 {
+    /**
+     * Load the routes for this bundle.
+     */
+    public function configureRoutes(RoutingConfigurator $routes): void
+    {
+        // Import your routes from config/routes.yaml in the bundle
+        $routes->import(__DIR__ . '/Resources/config/routes.yaml');
+    }
 }
